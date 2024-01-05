efe37f710061121f8908683853d2ec041dcccaa8
zBragaTo <leonardobragatobassi@hotmail.com>
2024-01-04 21:31:01 -0300

    first commit

---
+++ src/main/java/com/abctreinamentos/servidorpublicobdweb/ServidorpublicobdwebApplication.java
@@ -0,0 +1,13 @@
+package com.abctreinamentos.servidorpublicobdweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorpublicobdwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorpublicobdwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abctreinamentos/servidorpublicobdweb/ServidorpublicobdwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abctreinamentos.servidorpublicobdweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorpublicobdwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
